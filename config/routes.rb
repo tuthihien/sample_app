22c13b8232401ec779c00dc2ddf0ce88410dbadc
tuthihien <muaphun1138@gmail.com>
2016-11-24 09:55:23 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get  '/static_pages/about',   to: 'static_pages#about'
   get  '/static_pages/contact', to: 'static_pages#contact'
   get  'users/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
